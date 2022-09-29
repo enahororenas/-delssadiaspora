90809e8078d44b0dea3f1a10f4a4c52658829193
enahoroenas <orieroenahoro@yahoo.com>
2022-09-29 03:12:29 -0700

    added bday

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import {register, login, updateUser,updateUsersImage,getUsersImage,
-    addNewUserToDB,makeAUserAdmin} from '../controllers/authController.js'
+import {register, login, updateUser,updateUsersImage,
+    addNewUserToDB,makeAUserAdmin,getBday} from '../controllers/authController.js'
 import 'express-async-errors'
 import authenticateUser from '../middleware/auth.js'
 
@@ -10,7 +10,7 @@ router.route('/register').post(register)
 router.route('/login').post(login)
 router.route('/updateUser').patch(authenticateUser,updateUser)
 router.route('/updateImage').post(updateUsersImage)
-router.route('/getImage').post(authenticateUser,getUsersImage)
 router.route('/addnewuser').post(addNewUserToDB)
 router.route('/makeadmin').post(makeAUserAdmin)
+router.route('/getbday').get(getBday)
 export default router
