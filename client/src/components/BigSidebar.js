f51504f39f6e81591641bd5173fa515fa0fde20b
enahoroenas <orieroenahoro@yahoo.com>
2022-09-29 03:15:23 -0700

    added bday

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/BigSidebar'
 import { useAppContext } from '../context/appContext'
-import Logo from './Logo'
 import NavLinks from './NavLinks'
 
 
