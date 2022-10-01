aa1fba4e188c8dfe3d6af388770f79fd302e86e6
enahoroenas <orieroenahoro@yahoo.com>
2022-10-01 16:37:27 -0700

    updated galelery

--- client/src/pages/dashboard/Gallery.js
+++ client/src/pages/dashboard/Gallery.js
@@ -1,9 +1,9 @@
 import React,{useEffect} from 'react'
 import Wrapper from '../../assets/wrappers/Gallery'
-import {Navbar,BigSidebar,SmallSidebar} from '../../components'
+import {Gallerynavbar,Slideshow} from '../../components'
 import { useAppContext } from '../../context/appContext'
 import Loading from '../../components/Loading'
-//import NoJobsWrapper from '../../assets/wrappers/JobsContainer'
+
 
 const Gallery = () => {
 const {getImages,isLoading,urls} = useAppContext()
@@ -13,30 +13,17 @@ useEffect(()=>{
 },[])
 
 
-
 if(isLoading){return <Loading center/>}
-// /<Outlet/>
+
   return (
     <Wrapper>
-        <div className='navcontrol'><Navbar top='Gallery'/></div>
-          <div className='gcontainer'>
-         <div><SmallSidebar/>
-          <BigSidebar/>  </div>
-            
-          <div className='subgcontainer'>
-              {urls.length !== 0 &&
-                urls.map((imageUrl, index) => { 
-                  return  (
-                   <div key={index}>
-                    <img className='galleryimg' key={index} src={imageUrl} alt='...'></img>
-                    </div>
-                      )
-                    })} 
-              </div>
-              {urls.length === 0 && <h1 style={{fontWeight:'bold'}}>No Image Available</h1> }                
-         </div>
-      
-      
+        <div className='navcontrol'><Gallerynavbar top='Gallery'/></div>
+        <div className="imageparent">
+          
+          {urls.length !== 0 && <Slideshow imgs={urls}/> } 
+          {urls.length === 0 && <h1 style={{fontWeight:'bold'}}>No Image Available</h1> }
+          
+       </div>
     </Wrapper>
   )
 }
