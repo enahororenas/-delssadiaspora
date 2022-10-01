055e64a9bb0463ff6385dab2c1618acef50b2d0e
enahoroenas <orieroenahoro@yahoo.com>
2022-10-01 16:33:22 -0700

    updated galelery

--- client/src/assets/wrappers/Gallery.js
+++ client/src/assets/wrappers/Gallery.js
@@ -4,26 +4,47 @@ const Wrapper = styled.section`
   
  .gcontainer {
   display: grid;
-  grid-template-columns: 1fr 9fr;
+  grid-template-columns: 1fr 10fr;  
+  justify-content: center;
+  align-content: center;
+  background-color: red;
  }
+
+ .item-center {
+  display: grid;
+  grid-auto-flow: column;
+  gap: 4px;
+  align-items: center;
+  justify-items: center;
+}
 
  .subgcontainer {
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr;
   margin-left: 20px;
   margin-top: 10px;
   margin-right: 10px;
+  background-color: blue;
+  justify-items: center;
+  max-width: 100%;
+  max-height: 100%;
  }
 
  .galleryimg{
-  width: 95%;
-  height: 90%;
+  margin-top: 20px;
+  width: 70%;
+  height: 70%;
   border: 10px solid var(--primary-300);
  }
 
- .sidebar-container {
-  
- }
+ .imageparent {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  margin-top: 20px;
+}
 
 `
 export default Wrapper
