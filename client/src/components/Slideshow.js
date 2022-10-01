58ca5a592560835a91031d7e8c40d3e66e43461d
enahoroenas <orieroenahoro@yahoo.com>
2022-10-01 16:36:38 -0700

    updated galelery

---
+++ client/src/components/Slideshow.js
@@ -0,0 +1,42 @@
+import React,{useState,useEffect} from 'react'
+import Thumbnail from './Thumbnail'
+import Wrapper from '../assets/wrappers/Slideshow'
+
+const Slideshow = ({ imgs }) => {
+    const [index, setIndex] = useState(0)
+  
+    useEffect(() => {
+      setIndex(0)
+    }, [])
+  
+    const next = () => {
+      if (index === imgs.length - 1) {
+        setIndex(0)
+      } else {
+        setIndex(index + 1)
+      }
+    }
+    const prev = () => {
+      if (index === 0) {
+        setIndex(imgs.length - 1)
+      } else {
+        setIndex(index - 1)
+      }
+    }
+  
+  
+    return (
+    <Wrapper>
+    <div className="slideshow">
+      <img className="mainImg" src={imgs[index]} />
+      <div className="actions">
+        <button onClick={prev}>👈</button>
+        <button onClick={next}>👉</button>
+      </div>
+      <Thumbnail arr={imgs} image={setIndex} index={index} />
+    </div>
+      </Wrapper>
+    )
+  }
+
+export default Slideshow
