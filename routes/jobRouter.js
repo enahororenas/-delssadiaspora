f7683a3872089fc80b3e15a15edaa9383e5de3b2
enahoroenas <orieroenahoro@yahoo.com>
2022-10-02 16:39:08 -0700

    added comments

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,14 +1,15 @@
 import express from 'express'
-import {createJob, deleteJob, getAllJobs, updateJob,sendEmail,addImage,getAllImages,
-    addNews,getNews,getAllMembers,deleteNews} from '../controllers/jobsController.js'
+import {createComment,sendEmail,addImage,getAllImages,updateComment,deleteComment,
+    addNews,getNews,getAllMembers,deleteNews,getComments} from '../controllers/jobsController.js'
 const router = express.Router()
-
 router.route('/sendemail').post(sendEmail)
 router.route('/addimage').post(addImage).get(getAllImages)
 router.route('/addnewsitem').post(addNews).get(getNews)
 router.route('/getmembers').get(getAllMembers)
 router.route('/deletenewsitem').post(deleteNews)
-//router.route('/').post(createJob).get(getAllJobs)
-//router.route('/:id').delete(deleteJob).patch(updateJob)
+router.route('/add').post(createComment)
+router.route('/get').post(getComments)
+router.route('/update').post(updateComment)
+router.route('/delete').post(deleteComment)
 
 export default router
