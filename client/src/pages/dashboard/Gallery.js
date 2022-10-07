0812875a1f03e4c4a11076779a3ff02617d3ca49
enahoroenas <orieroenahoro@yahoo.com>
2022-10-07 09:18:49 -0700

    modified structire

--- client/src/pages/dashboard/Gallery.js
+++ client/src/pages/dashboard/Gallery.js
@@ -3,23 +3,21 @@ import Wrapper from '../../assets/wrappers/Gallery'
 import {Gallerynavbar,Slideshow} from '../../components'
 import { useAppContext } from '../../context/appContext'
 import Loading from '../../components/Loading'
-import ImageBtnContainer from '../../components/ImageBtnContainer'
+
 
 
 const Gallery = () => {
-const {getImages,isLoading,urls,getComments,new_comment,numOfImagePage,imagePage} = useAppContext()
+const {getImages,isLoading,urls,getComments,new_comment,imagePage} = useAppContext()
 
 useEffect(()=>{
   getImages()
   getComments()
 },[new_comment])
 
-
 const itemsPerPage = 10
 const indexOfLastItem =imagePage * itemsPerPage
 const indexOfFirstItem = indexOfLastItem - itemsPerPage
 const currentItems = urls.slice(indexOfFirstItem,indexOfLastItem)
-
 
 if(isLoading){return <Loading center/>}
 
@@ -28,14 +26,10 @@ if(isLoading){return <Loading center/>}
         <div className='navcontrol'><Gallerynavbar top='Gallery'/></div>
         
         <div className="imageparent"> 
-          {/*{urls.length !== 0 && <Slideshow imgs={urls}/> } */}
           {/*{urls.length === 0 && <h1 style={{fontWeight:'bold'}}>No Image Available</h1> }*/}
           {currentItems.length !== 0 && <Slideshow imgs={currentItems}/> } 
        </div>
-       <div>
-        {numOfImagePage > 1 && <ImageBtnContainer />}
-        </div>
-        
+
        
     </Wrapper>
   )
