3264801ccb01ecab355ad144159bf894e97792f6
enahoroenas <orieroenahoro@yahoo.com>
2022-10-07 19:03:29 -0700

    changed news structure

--- client/src/components/Newsitem.js
+++ client/src/components/Newsitem.js
@@ -4,17 +4,22 @@ import Wrapper from '../assets/wrappers/Newsitem'
 const Newsitem = ({header,img,article}) => {
   return (
     <Wrapper>
-        <div className='dashboard-page'>
-            <h2 className='h-center'>{header}</h2>
-            <div className='newscont'> 
-            {article}
-            </div>
-            {img && 
-            <div className="article-img">
-            <img className='liimg' src={img} alt="" />
-            </div>
-            }  
-        </div>
+        
+{img && 
+<div className='divwrapper'> 
+<img className='imgbdy' src={img} alt="" />
+<h2 className='h-center'>{header}</h2>
+<p className='par'>{article}</p>
+</div> 
+}
+  
+{!img && 
+<div className='divwrapper2'> 
+<h2 className='h-center'>{header}</h2>
+<p className='par'>{article}</p>
+</div> 
+}
+
     </Wrapper>
   )
 }
