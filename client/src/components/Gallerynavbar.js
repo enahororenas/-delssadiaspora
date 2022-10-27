3c7fb649b4d93b3996234a358d32a9d39cac92fb
enahoroenas <orieroenahoro@yahoo.com>
2022-10-27 05:08:36 -0700

    restructured

--- client/src/components/Gallerynavbar.js
+++ client/src/components/Gallerynavbar.js
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom'
 
 
 const Gallerynavbar = ({top}) => {
-    const {toggleSidebar,logoutUser,user} = useAppContext()
+    const {logoutUser,user} = useAppContext()
     const [showLogout,setShowLogout] = useState(false)
     const navigate = useNavigate()
   return (
