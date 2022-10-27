edac67c5d6fac353029a023c679e806413d711aa
enahoroenas <orieroenahoro@yahoo.com>
2022-10-27 08:44:45 -0700

    changed font size

--- client/src/assets/wrappers/Members.js
+++ client/src/assets/wrappers/Members.js
@@ -2,22 +2,19 @@ import styled from 'styled-components'
 
 const Wrapper = styled.article`
   .homebtn{
-    margin: 20px;
     width:fit-content;
-    height: auto;
     float:right;
-    font-size: 30px;
-  }
+    margin: 20px;
+    }  
 
   @media (max-width: 992px) {
-  .textwait{
-    font-size: medium;
-    }
+    
   }  
   @media (min-width: 992px) {
     .homebtn{
-      font-size: 40px;
-    }
+    height: auto;
+    font-size: 30px;
+   }
   }  
 `
 
